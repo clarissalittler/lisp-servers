09b0d402c005999c93b72cd7d4d10c86ea4887a8
Clarissa Littler <clarissa.littler@gmail.com>
2015-10-11 10:44:16 -0700

    added the teensiest example that still did something dynamic

---
+++ counter.lisp
@@ -0,0 +1,27 @@
+(ql:quickload '(cl-who hunchentoot parenscript))
+
+(defpackage :counter-server 
+  (:use :cl :cl-who :hunchentoot :parenscript))
+
+(in-package :counter-server)
+
+(defparameter counter 0)
+
+(setf (html-mode) :html5)
+
+(defun main-page () 
+  (with-html-output-to-string (*standard-output* nil :prologue t :indent t)
+    (:html 
+     (:head 
+      (:title "A Counter Example"))
+     (:body 
+      (:p (fmt "The counter is at ~d" counter))
+      (:form :action "/inc" :method "post"
+	     (:button "Click here!"))))))
+
+(define-easy-handler (counter-page :uri "/") () 
+    (main-page))
+
+(define-easy-handler (counter-inc :uri "/inc") ()
+    (incf counter)
+    (redirect "/"))
