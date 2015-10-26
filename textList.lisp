256bf1ca89a07e8ea06714aa3c132f681331b788
Clarissa Littler <clarissa.littler@gmail.com>
2015-10-25 18:57:35 -0700

    I have no idea why the static middleware doesn't work the way I expect

--- textList.lisp
+++ textList.lisp
@@ -1,4 +1,4 @@
-(ql:quickload '(cl-who parenscript clack ningle))
+(ql:quickload '(cl-who parenscript clack ningle lack))
 
 (defpackage :text-list-server
   (:use :cl :cl-who))
@@ -6,23 +6,14 @@
 (in-package :text-list-server)
 
 (defparameter text-list nil)
-(defparameter )
 
 (setf (html-mode) :html5)
 
 (defvar *app* (make-instance 'ningle:<app>))
 
-(defun file->string (filename)
-  (with-open-file (f filename)
-    (with-output-to-string (out)
-      (loop for line = (read-line f nil 'foo)
-	   until (eq line 'foo)
-	   do (write-line line out)))))
-
 ;;; so routes in ningle appear to be really similar to routes in something like Express
 ;;; let's try making this work with a little bit of backbone code that already exists
 
-(setf (ningle:route *app* "/")
-      (file->string "textlistServe.html"))
+(lack:builder (:static :path "/") *app*)
 
-(setf (ningle:route *app* ))
+(clack:clackup *app*)
